7ef6b5760a13d23798a5d22e655847251a64b1fb
ryuzo yajima <yajima.r@impl.co.jp>
2022-09-09 23:55:32 +0900

    add: ログイン画面作成

--- react/src/index.js
+++ react/src/index.js
@@ -14,9 +14,6 @@ root.render(
     <App/>
   </BrowserRouter>
   </ChakraProvider>
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- react/src/pages/login.jsx
+++ react/src/pages/login.jsx
@@ -1,5 +1,7 @@
 //Library
-import { Button, Text, Image, Center, Box, Input } from '@chakra-ui/react'
+import { Button, Text, Image, Center, Box, Input, Flex} from '@chakra-ui/react'
+import { Link as ChakraLink } from "@chakra-ui/react"
+import { Link as RouterLink } from "react-router-dom"
 
 //image
 import logoImage from "../common/public/logo.png";
@@ -9,29 +11,37 @@ function Login() {
     return (
     <Box w='100%' h='100vh' bgSize='cover' bgImage={loginBackground}>
         <Center>
-            <Box backdropFilter='blur(6px)' w={inputSize} p="30px">
+            <Flex position='absolute' top='20%' flexDirection='column' alignItems='center' justifyContent='space-around'>
                 <Image src={logoImage}
-                    boxSize='100px'
+                    boxSize='150px'
+                    position='relative'
+                    top='40px'
+                    zIndex='1'
                 />
-                <Box w={inputSize}>
-                    <Text color="white">Username</Text>
-                    <Input
-                        type="email"
-                        placeholder="test@test.com"
-                        w={inputSize}
-                        bgColor="white"
-                    ></Input>
-                    <Text color="white">Password</Text>
-                    <Input bgColor="white" type="password"></Input>
-                    <Button colorScheme="purple" w={inputSize}>LOGIN</Button>
+                <Box backdropFilter='blur(6px)' w={boxWidthSize} h={boxHeightSize} textAlign='center' >
+                    <Box w={inputWidthSize} m='auto'>
+                        <Text color="white" textAlign="left" py="2">Username</Text>
+                        <Input
+                            type="email"
+                            placeholder="test@test.com"
+                            w={inputWidthSize}
+                            bgColor="white"
+                        ></Input>
+                        <Text color="white" py="2" textAlign="left">Password</Text>
+                        <Input bgColor="white" type="password"></Input>
+                        <Button colorScheme="purple" w={inputWidthSize} my="4">LOGIN</Button>
+                        <ChakraLink as={RouterLink} to='/' color="white" fontSize="sm">新規会員登録はこちら</ChakraLink>
+                    </Box>
                 </Box>
-            </Box>
+            </Flex>
         </Center>
     </Box>
     );
 }
 
 //レスポンシブ対応 参考 https://zenn.dev/terrierscript/books/2021-05-chakra-ui/viewer/1-5-1-responsive
-const inputSize ={sm: "sm", md: "md", lg: "lg", xl: "xl"};
+const inputWidthSize = { base: "20em", sm: "sm", md: "md", lg: "lg", xl: "xl"};
+const boxWidthSize = { base: "22em", sm: "25em", md: "35em", lg: "40em", xl: "44em"};
+const boxHeightSize = { base: "17em", sm: "17em", md: "17em", lg: "17em", xl: "17em"};
 
 export default Login;
